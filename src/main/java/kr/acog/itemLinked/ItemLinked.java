d953ce33b7286778d1d4ce92e93928a6bbb5ac85
hongseongmin <acog.seongmin@gmail.com>
2023-02-09 01:11:02 +0900

    Legacy api Version fix

--- src/main/java/kr/acog/itemLinked/ItemLinked.java
+++ src/main/java/kr/acog/itemLinked/ItemLinked.java
@@ -21,16 +21,18 @@ public class ItemLinked extends JavaPlugin implements Listener {
     private String link;
     private String message;
     private String type;
-    private String color;
+    private String itemColor;
+    private String npeColor;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
         try {
             link = getConfig().getString("options.item-linked", "[item]");
+            npeColor = getConfig().getString("option.npe-message-color", "#B02C3A");
             message = getConfig().getString("options.npe-item-message", "You have no items in your hand.");
             type = getConfig().getString("options.npe-message-type", "actionbar");
-            color = getConfig().getString("options.color", "#EC4D2B");
+            itemColor = getConfig().getString("options.item-color", "#B5D1B6");
         } catch (NullPointerException npe) {
             getLogger().log(Level.SEVERE, "There is a problem with the form in Config.yml");
             saveDefaultConfig();
@@ -46,41 +48,42 @@ public class ItemLinked extends JavaPlugin implements Listener {
             return;
         }
         ItemStack handItem = event.getPlayer().getInventory().getItemInMainHand();
+
         if (handItem.getType() == Material.AIR) {
-            if (type.equals("message")) player.sendMessage(getError(this.message));
-            else player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(getError(this.message)));
+            if (type.equals("message")) player.sendMessage(color(npeColor, this.message));
+            else player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(color(npeColor, this.message)));
             return;
         }
         event.setCancelled(true);
         String format = String.format("%s: ", event.getFormat().split(":")[0]);
-        String display = handItem.getItemMeta().getDisplayName().equals("")
-                ? handItem.getType().name()
-                : handItem.getItemMeta().getDisplayName();
-
         TextComponent component = new TextComponent(format);
+        String display = handItem.getItemMeta().getDisplayName().equals("")
+                ? getItemTranslationKey(handItem)
+                : color(itemColor, handItem.getItemMeta().getDisplayName());
+
         TextComponent componentItem = new TextComponent("[");
-        componentItem.addExtra(new TranslatableComponent(getItemTranslationKey(handItem))); // 번역 안돼면 [] 빼기
+        componentItem.addExtra(new TranslatableComponent(display)); // 번역 안돼면 [] 빼기
         componentItem.addExtra(new TextComponent("]"));
-        componentItem.setColor(ChatColor.of(color));
+        componentItem.setColor(ChatColor.of(itemColor));
         componentItem.setHoverEvent(getItemHoverFrom(handItem));
 
         if (message.equals(link)) {
             component.addExtra(componentItem);
-        } else {
-            String[] messages = message.split("\\[item]");
-            for (int i = 0; i < messages.length; i++) {
-                component.addExtra(messages[i]);
-                if (i != messages.length - 1 || message.endsWith(link)) {
-                    component.addExtra(componentItem);
-                }
-            }
+            player.spigot().sendMessage(component);
+            return;
         }
 
+        String[] messages = message.split("\\[item]");
+        for (int i = 0; i < messages.length; i++) {
+            component.addExtra(messages[i]);
+            if (i != messages.length - 1 || message.endsWith(link)) {
+                component.addExtra(componentItem);
+            }
+        }
         player.spigot().sendMessage(component);
-        player.spigot().sendMessage(new ComponentBuilder().create());
     }
 
-    private String getError(String message) { return ChatColor.of("#EC4D2B") + message; }
+    private String color(String color, String message) { return ChatColor.of(color) + message; }
 
     public HoverEvent getItemHoverFrom(ItemStack bukkitItem) {
         net.minecraft.world.item.ItemStack item = CraftItemStack.asNMSCopy(bukkitItem);
@@ -93,4 +96,5 @@ public class ItemLinked extends JavaPlugin implements Listener {
     private String getItemTranslationKey(ItemStack bukkitItem) {
         return CraftItemStack.asNMSCopy(bukkitItem).p();
     }
+
 }
