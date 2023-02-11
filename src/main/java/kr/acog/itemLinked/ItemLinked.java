e393e5699cf4d5a4c7cb87873d15489e47c97c0b
hongseongmin <acog.seongmin@gmail.com>
2023-02-11 15:03:54 +0900

    fix: Code refactor & Bug fix

--- src/main/java/kr/acog/itemLinked/ItemLinked.java
+++ src/main/java/kr/acog/itemLinked/ItemLinked.java
@@ -1,22 +1,28 @@
 package kr.acog.itemLinked;
 
-import net.md_5.bungee.api.chat.HoverEvent;
-import net.md_5.bungee.api.chat.ItemTag;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.*;
 import net.md_5.bungee.api.chat.hover.content.Item;
 import net.minecraft.core.IRegistry;
+import org.bukkit.Material;
 import org.bukkit.craftbukkit.v1_19_R1.inventory.CraftItemStack;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Level;
 
 public class ItemLinked extends JavaPlugin implements Listener {
-    public static String link;
-    public static String message;
-    public static String type;
-    public static String itemColor;
-    public static String npeColor;
+    public String itemLink;
+    public String itemLinkMessageColor;
+    public String npeMessage;
+    public String npeMessageType;
+    public String npeMessageColor;
 
     public static HoverEvent getItemHoverFrom(ItemStack bukkitItem) {
         net.minecraft.world.item.ItemStack item = CraftItemStack.asNMSCopy(bukkitItem);
@@ -34,16 +40,70 @@ public class ItemLinked extends JavaPlugin implements Listener {
     public void onEnable() {
         saveDefaultConfig();
         try {
-            link = getConfig().getString("options.item-linked", "[item]");
-            npeColor = getConfig().getString("option.npe-message-color", "#B02C3A");
-            message = getConfig().getString("options.npe-item-message", "You have no items in your hand.");
-            type = getConfig().getString("options.npe-message-type", "actionbar");
-            itemColor = getConfig().getString("options.item-color", "#B5D1B6");
+            itemLink = getConfig().getString("options.item-linked", "[item]");
+            npeMessageColor = getConfig().getString("option.npe-message-color", "#B02C3A");
+            npeMessage = getConfig().getString("options.npe-item-message", "You have no items in your hand.");
+            npeMessageType = getConfig().getString("options.npe-message-type", "actionbar");
+            itemLinkMessageColor = getConfig().getString("options.item-color", "#B5D1B6");
         } catch (NullPointerException npe) {
             getLogger().log(Level.SEVERE, "There is a problem with the form in Config.yml");
             saveDefaultConfig();
         }
         getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onChat(AsyncPlayerChatEvent event) {
+        Player player = event.getPlayer();
+        String message = event.getMessage();
+        if (!message.toUpperCase().contains(itemLink.toUpperCase())) {
+            return;
+        }
+
+        ItemStack handItem = event.getPlayer().getInventory().getItemInMainHand();
+        if (handItem.getType() == Material.AIR) {
+            if (npeMessageType.equals("message")) {
+                player.sendMessage(ChatColor.of(npeMessageColor) + npeMessage);
+            } else {
+                    player.spigot().sendMessage(
+                            ChatMessageType.ACTION_BAR,
+                            TextComponent.fromLegacyText(ChatColor.of(npeMessageColor) + npeMessage)
+                    );
+            }
+            return;
+        }
+
+        event.setCancelled(true);
+        String format = String.format("%s: ", event.getFormat().split(":")[0]);
+        String display = handItem.getItemMeta().getDisplayName().equals("")
+                ? getItemTranslationKey(handItem)
+                : ChatColor.stripColor(handItem.getItemMeta().getDisplayName());
+
+        TextComponent component = new TextComponent(TextComponent.fromLegacyText(format));
+        TextComponent componentItem = new TextComponent("[");
+
+        componentItem.addExtra(new TranslatableComponent(display));
+        componentItem.addExtra(new TextComponent("]"));
+        componentItem.setColor(ChatColor.of(itemLinkMessageColor));
+        componentItem.setHoverEvent(ItemLinked.getItemHoverFrom(handItem));
+
+
+        if (message.equals(itemLink)) {
+            component.addExtra(componentItem);
+            player.spigot().sendMessage(component);
+            return;
+        }
+
+        String[] messages = message.split(String.format("\\%s", itemLink));
+        for (int i = 0; i < messages.length; i++) {
+            component.addExtra(messages[i]);
+            if (i != messages.length - 1 || message.endsWith(itemLink)) {
+                component.addExtra(componentItem);
+            }
+        }
+
+        getLogger().info(ChatColor.stripColor(component.toLegacyText()));
+        player.spigot().sendMessage(component);
     }
 
 }
